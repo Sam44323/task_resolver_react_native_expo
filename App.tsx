1b2fe8509894b7ca7becbc9559eac3887f6687ea
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-22 23:58:06 +0530

    add: added font support for nunito

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
 import { StatusBar } from "expo-status-bar";
+import Fonts from "expo-font";
 import { StyleSheet, Text, View } from "react-native";
+import Home from "./screens/home";
+import About from "./screens/about";
+import ReviewDetails from "./screens/reviewDetails";
+
+const getFonts = () => {
+  return Fonts.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Home />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
