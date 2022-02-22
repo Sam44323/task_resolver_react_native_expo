65d4390496f2bf42d7a2f0044cf5c9c76ddf6e01
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-23 00:58:28 +0530

    add: added back navigation_handler

--- screens/reviewDetails.tsx
+++ screens/reviewDetails.tsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/globals";
 
-const ReviewDetails = () => {
+const ReviewDetails = ({ navigation }: any) => {
+  const backNavigationHandler = () => navigation.goBack();
+
   return (
     <View style={globalStyles.container}>
       <Text>ReviewDetails</Text>
+      <Button title="Back to Home Screen" onPress={backNavigationHandler} />
     </View>
   );
 };
