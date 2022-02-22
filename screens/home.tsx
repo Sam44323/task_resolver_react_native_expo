bf90eabaad536d4bae79cdcede2da26ae88147df
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-23 00:57:03 +0530

    add: added button stack-pusher for navigation

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/globals";
 
-const Home = () => {
+const Home = ({ navigation }: any) => {
+  const navigationHandler = () => {
+    navigation.push("review");
+  };
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Home</Text>
+      <Button title="Go to review page" onPress={navigationHandler} />
     </View>
   );
 };
