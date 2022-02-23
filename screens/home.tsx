8f0b82587fb7655483b19367d2615cca85be648b
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-23 21:50:39 +0530

    add: added modal_support for form

--- screens/home.tsx
+++ screens/home.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import { globalStyles } from "../styles/globals";
 import Card from "../shared/card";
 
 const Home = ({ navigation }: any) => {
+  const [modalOpen, setModalOpen] = React.useState(false);
   const [reviews, setReviews] = React.useState([
     {
       title: "title #1",
@@ -27,6 +36,24 @@ const Home = ({ navigation }: any) => {
 
   return (
     <View style={globalStyles.container}>
+      <Modal visible={modalOpen} animationType="slide">
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name="close"
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <Text>Hello from the modal :)</Text>
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name="add"
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
       <FlatList
         data={reviews}
         renderItem={({ item }) => {
@@ -44,5 +71,25 @@ const Home = ({ navigation }: any) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  modalToggle: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "#f2f2f2",
+    padding: 10,
+    borderRadius: 10,
+    alignSelf: "center",
+  },
+  modalClose: {
+    marginTop: 20,
+    marginBottom: 0,
+  },
+  modalContent: {
+    flex: 1,
+  },
+});
 
 export default Home;
