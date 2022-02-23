9b7576e5027126e7b9e9f3fcad0679a27f99540b
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-23 21:57:30 +0530

    add: added initial setup for forms

--- screens/home.tsx
+++ screens/home.tsx
@@ -10,6 +10,7 @@ import {
 import { MaterialIcons } from "@expo/vector-icons";
 import { globalStyles } from "../styles/globals";
 import Card from "../shared/card";
+import ReviewForm from "./reviewForm";
 
 const Home = ({ navigation }: any) => {
   const [modalOpen, setModalOpen] = React.useState(false);
@@ -44,7 +45,7 @@ const Home = ({ navigation }: any) => {
             style={{ ...styles.modalToggle, ...styles.modalClose }}
             onPress={() => setModalOpen(false)}
           />
-          <Text>Hello from the modal :)</Text>
+          <ReviewForm />
         </View>
       </Modal>
 
