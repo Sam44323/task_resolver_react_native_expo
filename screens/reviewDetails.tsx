82abc3cb97540db71adaa2f9781803b6854d4bff
Suranjan <suranjanbiswas2002@gmail.com>
2022-02-23 21:39:18 +0530

    remove: removed unused imports from reviewDetails

--- screens/reviewDetails.tsx
+++ screens/reviewDetails.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/globals";
 import Card from "../shared/card";
 
